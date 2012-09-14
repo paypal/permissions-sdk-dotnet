5cec4e965be50a07470b369f1dece7d30be2c9fd
unknown <rsomaskandan@D-MAA-00398451.corp.ebay.com>
2012-09-14 15:22:22 +0530

    dotnet samples modified for test automation

--- Samples/PermissionsSampleApp/permissionshandler.ashx.cs
+++ Samples/PermissionsSampleApp/permissionshandler.ashx.cs
@@ -20,18 +20,19 @@ namespace PermissionsSampleApp
 
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.ContentType = "text/plain";
+            //Selenium Test Case
+            context.Response.ContentType = "text/html";
 
             String strCall = context.Request.Params["PermissionsBtn"];
 
-             if(strCall.Equals("RequestPermissions")){
+            if (strCall.Equals("RequestPermissions"))
+            {
                 RequestPermissions(context);
-            }else if(strCall.Equals("GetAccessToken")){
-                GetAccessToken(context);
-            
-                                
             }
-
+            else if (strCall.Equals("GetAccessToken"))
+            {
+                GetAccessToken(context);
+            }
         }
 
         public bool IsReusable
@@ -43,37 +44,27 @@ namespace PermissionsSampleApp
         }
 
 
-       
-
-       
-
-
-
         private void RequestPermissions(HttpContext context)
         {
-
-         RequestPermissionsRequest rp = new RequestPermissionsRequest();           
+            RequestPermissionsRequest rp = new RequestPermissionsRequest();
             rp.scope = new List<string>();
-          string[] scopes= context.Request.Form.GetValues("api");
-          
-            for (int i=0;i<scopes.Length;i++)
-            rp.scope.Add(scopes[i]);
+            string[] scopes = context.Request.Form.GetValues("api");
 
-            
+            for (int i = 0; i < scopes.Length; i++)
+                rp.scope.Add(scopes[i]);
+
+
             rp.callback = context.Request.Params["callback"];
             rp.requestEnvelope = new RequestEnvelope("en_US");
-           RequestPermissionsResponse rpr = null;
+            RequestPermissionsResponse rpr = null;
 
             try
             {
-               PermissionsService service = new PermissionsService();                
+                PermissionsService service = new PermissionsService();
                 rpr = service.RequestPermissions(rp);
                 context.Response.Write("<html><body><textarea rows=30 cols=80>");
-                ObjectDumper.Write(rpr, 5, context.Response.Output);               
+                ObjectDumper.Write(rpr, 5, context.Response.Output);
                 context.Response.Write("</textarea></body></html>");
-
-                context.Items["responseObject"] = rpr;
-                context.Items["responsePayload"] = service.getLastResponse();
 
                 string red = "<br>";
                 red = red + "<a href=";
@@ -85,13 +76,11 @@ namespace PermissionsSampleApp
             {
                 context.Response.Write(e.Message);
             }
-
         }
 
         private void GetAccessToken(HttpContext context)
         {
-            
-           GetAccessTokenRequest gat = new GetAccessTokenRequest();
+            GetAccessTokenRequest gat = new GetAccessTokenRequest();
 
             String token = context.Request.Params["txtrequest_token"];
             String verifier = context.Request.Params["txtverification_code"];
@@ -99,31 +88,57 @@ namespace PermissionsSampleApp
             gat.token = token;
             gat.verifier = verifier;
 
-
             gat.requestEnvelope = new RequestEnvelope("en_US");
-           GetAccessTokenResponse gats = null;
+            GetAccessTokenResponse gats = null;
 
             try
             {
                 PermissionsService service = new PermissionsService();
-                gats = service.GetAccessToken(gat);                
+                gats = service.GetAccessToken(gat);
                 context.Response.Write("<html><body><textarea rows=30 cols=80>");
                 ObjectDumper.Write(gats, 5, context.Response.Output);
-                context.Response.Write("</textarea></body></html>");
+                context.Response.Write("</textarea></br>");
 
-                context.Items["responseObject"] = gats;
-                context.Items["responsePayload"] = service.getLastResponse();
-       
+                //Selenium Test Case
+                context.Response.Write("</br>Acknowledgement: ");
+                context.Response.Write("<div id = '");
+                context.Response.Write("Acknowledgement");
+                context.Response.Write("'>");
+                context.Response.Write(gats.responseEnvelope.ack);
+                context.Response.Write("</div>");
+
+                context.Response.Write("</br>Request token: ");
+                context.Response.Write("<div id = '");
+                context.Response.Write("Request token");
+                context.Response.Write("'>");
+                context.Response.Write(context.Request.Params["txtrequest_token"]);
+                context.Response.Write("</div>");
+
+                context.Response.Write("</br>Verification code: ");
+                context.Response.Write("<div id = '");
+                context.Response.Write("Verification code");
+                context.Response.Write("'>");
+                context.Response.Write(context.Request.Params["txtverification_code"]);
+                context.Response.Write("</div>");
+                                
+                context.Response.Write("</br>token: ");
+                context.Response.Write("<div id = '");
+                context.Response.Write("token");
+                context.Response.Write("'>");
+                context.Response.Write(gats.token);          
+                context.Response.Write("</div>");
+
+                context.Response.Write("</br>tokenSecret: ");
+                context.Response.Write("<div id = '");
+                context.Response.Write("tokenSecret");
+                context.Response.Write("'>");
+                context.Response.Write(gats.tokenSecret);
+                context.Response.Write("</div>");
             }
             catch (System.Exception e)
             {
                 context.Response.Write(e.Message);
             }
-
         }
-
-
-
-        
     }
 }
