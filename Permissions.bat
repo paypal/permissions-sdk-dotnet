f3b7bea8a11c6c3b0d64ce6ddc6fd0f3ec88a4b7
unknown <rsomaskandan@D-MAA-00391891.corp.ebay.com>
2012-12-27 12:03:10 +0530

    Removed underscore from folders as part of .NET Standards

--- Permissions.bat
+++ Permissions.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_Permissions_SDK\PayPal_Permissions_SDK.sln /build Release %1
+call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPalPermissionsSDK\PayPalPermissionsSDK.sln /build Release
 
-copy /Y PayPal_Permissions_SDK\bin\Release\PayPalPermissionsSDK.dll Samples\PermissionsSampleApp\lib\. 
+copy /Y PayPalPermissionsSDK\bin\Release\PayPalPermissionsSDK.dll Samples\PermissionsSampleApp\lib\. 
