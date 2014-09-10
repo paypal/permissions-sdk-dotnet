2a1ef24e1af79afb35ad9942c0010ad72f336586
jziaja <jziaja@paypal.com>
2014-09-10 11:12:59 -0500

    NuGet package dependency fix

--- PayPalPermissionsSDK/Permissions/PermissionsService.cs
+++ PayPalPermissionsSDK/Permissions/PermissionsService.cs
@@ -31,7 +31,7 @@ namespace PayPal.Permissions
 		/// <summary>
 		/// SDK Version
 		/// </summary>
-		private const string SDKVersion = "2.4.109";
+		private const string SDKVersion = "2.5.109";
 
 		/// <summary>
 		/// Default constructor for loading configuration from *.Config file
